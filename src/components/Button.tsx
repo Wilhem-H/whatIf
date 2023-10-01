707da980e9b969319aff231883782d12063c541c
Wilhem-H <wilhem.hafsa@outlook.fr>
2023-10-01 23:33:35 +0200

    add contact page with desktop css

--- src/components/Button.tsx
+++ src/components/Button.tsx
@@ -3,12 +3,15 @@ import "./Button.css";
 export interface Props {
   message: string;
   color?: string;
+  type?: string;
 }
 
-export function Button({ message, color }: Props) {
+export function Button({ message, color, type }: Props) {
   return (
     <div className="button">
-      <button id={color}>{message}</button>
+      <button id={color} type={type ? "submit" : "button"}>
+        {message}
+      </button>
     </div>
   );
 }
--- src/pages/Contact.tsx
+++ src/pages/Contact.tsx
@@ -1,17 +1,122 @@
+import { useState } from "react";
 import { Link } from "react-router-dom";
 import { Button } from "../components/Button";
+import { Header } from "../components/Header";
+
+import cat from "/contact/cat.jpg";
+import aquatic from "/contact/aquatic.jpg";
+import eclipse3 from "/contact/eclipse3.jpg";
+import eclipse6 from "/contact/eclipse6.jpg";
+import eclipse7 from "/contact/eclipse7.jpg";
+import france from "/contact/france.jpg";
+import internet from "/contact/internet.jpg";
+import plant1 from "/contact/plant1.jpg";
+import poutine from "/contact/poutine.jpg";
+
+import TextField from "@mui/material/TextField";
 
 import "./Contact.css";
 
 export default function Contact() {
+  const [message, setMessage] = useState({
+    firstName: "",
+    lastName: "",
+    mail: "",
+    scenario: "",
+    details: "",
+  });
+
+  const randomImg = [
+    cat,
+    france,
+    eclipse3,
+    eclipse6,
+    eclipse7,
+    aquatic,
+    internet,
+    plant1,
+    poutine,
+  ];
+  const randomNumber = Math.floor(Math.random() * randomImg.length);
+  const randomImage = randomImg[randomNumber];
+
+  const handleInputChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const { name, value } = e.target;
+    setMessage((prevMessage) => ({
+      ...prevMessage,
+      [name]: value,
+    }));
+  };
+
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(message);
+  };
+
   return (
     <div className="contact">
+      <Header />
       <div className="contact-button">
         <Link to="/all-articles">
           <Button message="Menu" color="color" />
         </Link>
       </div>
-      <div className="contact-content"></div>
+      <div className="contact-content">
+        <div className="contact-content-img">
+          <img src={randomImage} alt="" />
+        </div>
+        <div className="contact-content-form">
+          <h3>Vous avez une idée de scénario à proposer ?</h3>
+          <h3>CONTACTEZ NOUS!</h3>
+          <form onSubmit={handleSubmit}>
+            <TextField
+              fullWidth
+              label="firstName*"
+              id="firstName"
+              name="firstName"
+              value={message.firstName}
+              onChange={handleInputChange}
+            />
+            <TextField
+              fullWidth
+              label="Prénom*"
+              id="lastName"
+              name="lastName"
+              value={message.lastName}
+              onChange={handleInputChange}
+            />
+            <TextField
+              fullWidth
+              label="Mail*"
+              id="mail"
+              name="mail"
+              value={message.mail}
+              onChange={handleInputChange}
+            />
+            <TextField
+              fullWidth
+              label="Scénario*"
+              id="scenario"
+              name="scenario"
+              value={message.scenario}
+              onChange={handleInputChange}
+            />
+
+            <TextField
+              fullWidth
+              id="details"
+              name="details"
+              label="Précision ?"
+              placeholder="Placeholder"
+              multiline
+              value={message.details}
+              onChange={handleInputChange}
+            />
+
+            <Button message="Valider" type="submit" />
+          </form>
+        </div>
+      </div>
     </div>
   );
 }
